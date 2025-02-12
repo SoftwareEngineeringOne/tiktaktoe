4b37095d607de428fb223d1bf340396b5ed0936b
DosKobold <hanspeter56@t-online.de>
2025-02-12 17:36:57 +0100

    feat: add field drawing algorithm

--- src/presentation/field.c
+++ src/presentation/field.c
@@ -1,3 +1,28 @@
 //
 // Created by paul on 2/10/25.
 //
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <presentation/field.h>
+#include <presentation/print.h>
+
+void output_drawField() {
+    const uint8_t length = 2*FIELD_SIZE+2;
+    char horizontal[length];
+    char vertical[length];
+
+    for (uint8_t x = 0; x < length; x++) {
+        horizontal[x] = (x%2) ? '-' : '+';
+        vertical[x] = (x%2) ? ' ' : '|';
+    }
+    horizontal[length-1] = '\0';
+    vertical[length-1] = '\0';
+
+    for (uint8_t y = 0; y < FIELD_SIZE; y++) {
+        println(horizontal);
+        println(vertical);
+        println(horizontal);
+    }
+}

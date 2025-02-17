2db41db8a5c958b098eaf8ccd599ae1b08ac83e1
DosKobold <hanspeter56@t-online.de>
2025-02-17 17:39:45 +0100

    feat: add structure for cell-management

--- include/logic/input.h
+++ include/logic/input.h
@@ -2,7 +2,8 @@
 #define INPUT_H
 
 #include "logic/game.h"
+#include "presentation/field.h"
 
-void input_handleEscapeSequence(Cell *selected_cell);
+void input_handleEscapeSequence(Cell *cells[CELLS_PER_COL][CELLS_PER_ROW], Cell *selected_cell);
 
 #endif// !INPUT_H

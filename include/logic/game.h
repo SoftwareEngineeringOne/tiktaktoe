e3156f2dedf99ec030cd331df49171ea68391844
DosKobold <hanspeter56@t-online.de>
2025-02-18 16:10:18 +0100

    feat: add algorithm for victory

--- include/logic/game.h
+++ include/logic/game.h
@@ -11,6 +11,8 @@
 
 #define REMAINING_TIME TICKS_PER_ROUND - time_roundTicks
 
+#define min(a, b) a < b ? a : b
+
 typedef enum
 {
     Human,
@@ -34,5 +36,9 @@ static Cell* bot_cell;
 void game_run();
 
 void game_onTimeOut();
+
+void game_checkGameState();
+
+uint8_t game_playerHasWon(uint8_t x, uint8_t y, uint8_t cellsToWin, Mark mark);
 
 #endif// GAME_H

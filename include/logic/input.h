066fb8d586c01c4133d62f33ce8ee9197056ede0
Simon <simon21.blum@gmail.com>
2025-02-18 14:19:06 +0100

    feat: add basic menu

--- include/logic/input.h
+++ include/logic/input.h
@@ -4,6 +4,6 @@
 #include "logic/game.h"
 #include "presentation/field.h"
 
-void input_handleEscapeSequence(Cell cells[CELLS_PER_ROW][CELLS_PER_COL], Cell **selected_cell);
+void input_handleEscapeSequence(Cell cells[CELLS_PER_COL][CELLS_PER_ROW], Cell **selected_cell);
 
 #endif// !INPUT_H
